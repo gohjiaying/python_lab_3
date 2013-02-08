4f212a4d39dd73d63db5a31135b0e31934376c07
goh.jiaying1 <goh.jiaying1@dhs.sg>
2013-02-08 12:47:26 +0800

    to get a perfect text

--- decipher.py
+++ decipher.py
@@ -1,30 +1,33 @@
 #filename: Decipher.py
 #author: Goh Jia ying
-
+def ascii_shift(string):
+        
+    new_string = ""
+    for i in string:
+        new_chr_no = ord(i) - 5
+        new_chr = chr(new_chr_no)
+        new_string = new_string + new_chr
+    return (new_string)
+            
 try:
     mystery_file = open('MYSTERY.IN', 'r')
-    outfile = open('MYSTERY.TXT','r')
+    outfile = open('MYSTERY.TXT','w')
     line = mystery_file.readlines()
     
-    #def fibonnaci function:
+##    #def fibonnaci function:
 ##    def fibonacci_generator():
 ##        num = 1
 ##        new_num =
 
+
+
     #def ascii shift function, shift by 5
-    def ascii_shift(string):
-        str_string = str(string)
-        new_string = ""
-        for i in str_string:
-            new_chr_no = ord(i) -5
-            new_chr = chr(new_chr_no)
-            new_string = new_string + str(new_chr)
-        print(new_string)
-            
+    
     #try decripting...
-    text =  str(ascii_shift(line))
+    for row in line: # important line cannot directlyjust print line as \n will be printed
+        print(ascii_shift(row))
     word =  text.split(' ')
-    print(word + "\n")
+##    print(word + "\n")
 
     #def a count word function
 ##    def count_word(text,word):
@@ -37,3 +40,4 @@ try:
     
 except IOError:
     print("File MYSTERY.IN cannot be read or MYSTERY.TXT cannot be written")
+
